2d9d54c54769d37b2f2d20854138db2553bfe5ae
DhruvSvt <dhruvsvt08@gmail.com>
2023-11-09 15:24:43 +0530

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Controllers\LoginController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\CheckAdmin;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+
+Route::get('/login', [LoginController::class, 'index'])->name('login');
+
+Route::get('/', function () {
+    return view('admin.layouts.app');
+});
+Route::middleware([CheckAdmin::class])->group(function () {
+});
