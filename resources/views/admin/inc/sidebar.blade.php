29c84fbca81ae620b25c509beca494e6f54ec45b
DhruvSvt <dhruvsvt08@gmail.com>
2023-11-10 16:26:11 +0530

    Update sidebar.blade.php

--- resources/views/admin/inc/sidebar.blade.php
+++ resources/views/admin/inc/sidebar.blade.php
@@ -13,10 +13,10 @@
         <!-- Light Logo-->
         <a href="index.html" class="logo logo-light">
             <span class="logo-sm">
-                <img src="{{ config('app.url') }}assets/images/ganpati-logo.png" alt="" height="35">
+                <img src="{{ config('app.url') }}/assets/images/ganpati-logo.png" alt="" height="35">
             </span>
             <span class="logo-lg">
-                <img src="{{ config('app.url') }}assets/images/ganpati-logo.png" alt="" height="70">
+                <img src="{{ config('app.url') }}/assets/images/ganpati-logo.png" alt="" height="70">
             </span>
         </a>
         <button type="button" class="btn btn-sm p-0 fs-20 header-item float-end btn-vertical-sm-hover"
@@ -37,12 +37,14 @@
                 <li class="nav-item">
                     <a class="nav-link menu-link" href="#sidebarDashboards" data-bs-toggle="collapse" role="button"
                         aria-expanded="false" aria-controls="sidebarDashboards">
-                        <i class="ri-dashboard-2-line"></i> <span data-key="t-dashboards" style="font-size: 18px">Dashboards</span>
+                        <i class="ri-dashboard-2-line"></i> <span data-key="t-dashboards"
+                            style="font-size: 18px">Dashboards</span>
                     </a>
                     <div class="collapse menu-dropdown" id="sidebarDashboards">
                         <ul class="nav nav-sm flex-column">
                             <li class="nav-item">
-                                <a href="dashboard-analytics.html" class="nav-link" data-key="t-analytics" style="font-size: 15px;">
+                                <a href="dashboard-analytics.html" class="nav-link" data-key="t-analytics"
+                                    style="font-size: 15px;">
                                     Analytics </a>
                             </li>
                         </ul>
@@ -55,13 +57,22 @@
                 <li class="nav-item">
                     <a class="nav-link menu-link" href="#sidebarAdmins" data-bs-toggle="collapse" role="button"
                         aria-expanded="false" aria-controls="sidebarAdmins">
-                        <i class="ri-team-fill"></i> <span data-key="t-dashboards" style="font-size: 18px">Admin</span>
+                        <i class="ri-user-settings-fill"></i> <span data-key="t-dashboards"
+                            style="font-size: 18px">Admin</span>
                     </a>
                     <div class="collapse menu-dropdown" id="sidebarAdmins">
                         <ul class="nav nav-sm flex-column">
                             <li class="nav-item">
-                                <a href="dashboard-analytics.html" class="nav-link" data-key="t-analytics" style="font-size: 15px;">
+                                <a href="{{ route('members.index') }}" class="nav-link" data-key="t-analytics"
+                                    style="font-size: 15px;">
                                     Members </a>
+                            </li>
+                        </ul>
+                        <ul class="nav nav-sm flex-column">
+                            <li class="nav-item">
+                                <a href="dashboard-analytics.html" class="nav-link" data-key="t-analytics"
+                                    style="font-size: 15px;">
+                                    Society </a>
                             </li>
                         </ul>
                     </div>
