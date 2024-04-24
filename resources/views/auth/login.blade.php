efd25036b456f00c3cd5dec24d129e0636161c9b
deepakagarwal1995 <37587832+deepakagarwal1995@users.noreply.github.com>
2024-04-24 13:18:02 +0530

    anpati Society Admin

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8" />
     <title>Guruji IMF</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta content="Ganpati Society Admin" name="description" />
+    <meta content="Guruji IMF" name="description" />
     <!-- App favicon -->
     <link rel="shortcut icon" href="{{ config('app.url') }}/assets/images/logoicon.png">
     <!-- Layout config Js -->
